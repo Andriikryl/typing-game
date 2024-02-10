ca3d74b7f1cba2ec137b4bd4a69a78a702354909
Andriikryl <andreyk557@gmail.com>
2024-02-10 15:46:40 +0200

    add dependence to the game

--- src/components/TypingGame.svelte
+++ src/components/TypingGame.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
   type Game = "waiting for input" | "in progress" | "game over";
   type Word = string;
+  let game: Game = "waiting for input";
+  let typedLetter = "";
+  let words: Word[] = "The quick brown fox jumps over the lazy dog".split(" ");
+  let wordIndex = 0;
+  let letterIndex = 0;
+  let correctLetters = 0;
+  let wordsEl: HTMLDivElement;
+  let letterEl: HTMLSpanElement;
+  let inputEl: HTMLInputElement;
 </script>
 
 <div>
